a9a2a181af2853e95373f7545965012aa4bc4b27
Derk-Jan Karrenbeld <derk-jan+git@karrenbeld.info>
2013-03-14 20:36:28 +0100

    Added GameManager

---
+++ js/core/game.js
@@ -0,0 +1,42 @@
+var Game = new function() {
+	var objects = {};
+	var layers = {};
+
+	var stage = new Kinetic.Stage({
+		container: 'container',
+		width: 640,
+		height: 480
+	});
+	
+	// Adds an object
+	this.add = function( key, object ) {
+		if ( this.get( key ) !== undefined )
+			throw new Error;
+			
+		objects[ key ] = object;
+	};
+	
+	// Removes an object
+	this.remove = function( key ) {
+		delete objects[ key ];
+	};
+	
+	// Adds a layer
+	this.addLayer = function( key, layer ) {
+		layers[ key ] = layer;
+		this.getStage().add( layer );
+	};
+	
+	// Gets an object
+	this.get = function( key ) {
+		return objects[ key ];
+	};
+	
+	this.getStage = function() {
+		return stage;
+	};
+	
+	this.getLayer = function( layer ) {
+		return layers[ layer ];
+	};
+};
