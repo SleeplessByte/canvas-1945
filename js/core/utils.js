c67aa36d512d8bd0c08683dda339731b9efba2aa
Derk-Jan Karrenbeld <derk-jan+git@karrenbeld.info>
2013-03-15 01:27:52 +0100

    Added document hidden function

--- js/core/utils.js
+++ js/core/utils.js
@@ -20,3 +20,38 @@ Object.deepExtend = function(destination, source) {
 	}
 	return destination;
 };
+
+// Document hidden
+(function() {
+    var hidden = "hidden";
+
+    // Standards:
+    if (hidden in document)
+        document.addEventListener("visibilitychange", onchange);
+    else if ((hidden = "mozHidden") in document)
+        document.addEventListener("mozvisibilitychange", onchange);
+    else if ((hidden = "webkitHidden") in document)
+        document.addEventListener("webkitvisibilitychange", onchange);
+    else if ((hidden = "msHidden") in document)
+        document.addEventListener("msvisibilitychange", onchange);
+
+    // IE 9 and lower:
+    else if ('onfocusin' in document)
+        document.onfocusin = document.onfocusout = onchange;
+
+    // All others:
+    else
+        window.onfocus = window.onblur = onchange;
+
+    function onchange (evt) {
+        var body = document.body;
+        evt = evt || window.event;
+
+        if (evt.type == "focus" || evt.type == "focusin")
+            body.className = "visible";
+        else if (evt.type == "blur" || evt.type == "focusout")
+            body.className = "hidden";
+        else        
+            body.className = this[hidden] ? "hidden" : "visible";
+    }
+})();
