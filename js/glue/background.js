313fedeae834fe4b3f9be40b71d8542ed7f3ac80
Derk-Jan Karrenbeld <derk-jan+git@karrenbeld.info>
2013-03-15 03:32:15 +0100

    Added islands to a background file

---
+++ js/glue/background.js
@@ -0,0 +1,8 @@
+// Islands
+var island_one = new Island( sourceImage, 'one' );
+var island_two = new Island( sourceImage, 'two' );
+var island_three = new Island( sourceImage, 'three' );
+
+island_one.attach( Game.getLayer( 'background' ) );
+island_two.attach( Game.getLayer( 'background' ) );
+island_three.attach( Game.getLayer( 'background' ) );
