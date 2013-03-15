2f836352e518ce7a271cd5ad0d6f6aa8d6c6ca09
Derk-Jan Karrenbeld <derk-jan+git@karrenbeld.info>
2013-03-15 03:33:46 +0100

    Changed super class of playerplane
    BasicPlane is now a basic enemy plane. Player plane is special in a lot
    of ways.

--- js/models/planes/player.js
+++ js/models/planes/player.js
@@ -1,17 +1,41 @@
 var PlayerPlane = function ( srcimg ) {
-    BasicPlane.call( this, srcimg, 'player' );
+    Sprite.call( this, srcimg, 'player' );
+
+	var health = 100;
+	
+	// Returns the planes speed
+	this.getSpeed = function() {
+		return Game.getSpeed();
+	};
+	
+	// Returns the planes health
+	this.getHealth = function() {
+		return health;
+	};
 }
 
 PlayerPlane.prototype = Object.deepExtend( 
-	Object.create( BasicPlane.prototype ),
+	Object.create( Sprite.prototype ),
 	{ 
 		// Add animations (frame positions)
 		getFramePositions : function() {
 			return { 
 				idle : {
 					player : { x: 4,	y: 400  },
+				},
+				
+				explode : {
+					player : { x: 4,	y: 301	},
 				}
 			};
+		},
+		
+		// The animation length (number of frames)
+		getFrameLength : function() {
+			return { 
+				idle : { x: 3, y: 1 },
+				explode : { x: 7, y: 1 },
+			}
 		},
 		
 		// The size of each animation frame
