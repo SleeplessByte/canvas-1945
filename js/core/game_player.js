dd978f3359780421287768ae90ba42502c3a3604
Derk-Jan Karrenbeld <derk-jan+git@karrenbeld.info>
2013-03-15 05:14:24 +0100

    Game Player input handling as closure

--- js/core/game_player.js
+++ js/core/game_player.js
@@ -35,6 +35,7 @@ var GamePlayer = new function() {
 	// Spawns the player
 	this.spawn = function( newname, sourceImage ) {
 	
+		// Hello player
 		name = newname;
 		plane = new PlayerPlane( sourceImage );
 		plane.attach( Game.getLayer( 'level' ) );
@@ -44,23 +45,46 @@ var GamePlayer = new function() {
 			Game.getCenterStage().x - plane.getWidth() / 2,
 			400
 		);
-		
+				
 		// Player animation loop
-		var context = plane.update;
-		plane.update = function( frame ) { 
-			this.updatePosition();
-			
-			var delta = plane.getSpeed() * frame.timeDiff / 1000;
-			
-			if ( Input.IsKeyDown( 38 ) ) // up
-				plane.deltaPosition( 0, -delta );
-			if ( Input.IsKeyDown( 40 ) ) // down
-				plane.deltaPosition( 0, delta );
-			if ( Input.IsKeyDown( 37 ) ) // left
-				plane.deltaPosition( -delta, 0 );
-			if ( Input.IsKeyDown( 39 ) ) // right
-				plane.deltaPosition( delta, 0 );
-		};
+		plane.update = (function() {
+		
+			// Returns a clamped position
+			var clampPosition = function( width, height, position ) {
+				position.x = Math.min( 
+					Math.max( position.x, 10 ), 
+					Game.getStage().getWidth() - 10 - width 
+				);
+				position.y = Math.min(
+					Math.max( position.y, 10 ),
+					Game.getStage().getHeight() - 10 - height
+				);
+				return position;
+			}
+		
+			var context = plane.update;
+			return function( frame ) { 
+				
+				var delta = plane.getSpeed() * frame.timeDiff / 1000;
+				
+				// Movement by keys
+				if ( Input.IsKeyDown( 38 ) ) // up
+					plane.deltaPosition( 0, -delta );
+				if ( Input.IsKeyDown( 40 ) ) // down
+					plane.deltaPosition( 0, delta );
+				if ( Input.IsKeyDown( 37 ) ) // left
+					plane.deltaPosition( -delta, 0 );
+				if ( Input.IsKeyDown( 39 ) ) // right
+					plane.deltaPosition( delta, 0 );
+					
+				// Clamp the position
+				var position = clampPosition( this.getWidth(), this.getHeight(), this.getPosition() );
+				this.setPosition( position.x, position.y );
+				
+				// Update the position
+				context.call( this, frame );
+			};
+		})();
 	};	
 	
 	// Despawns the player
