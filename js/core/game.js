23a707cc4567025693576b95453987352948f751
Derk-Jan Karrenbeld <derk-jan+git@karrenbeld.info>
2013-03-15 02:31:29 +0100

    FPS to game loop and fixed updating stage

--- js/core/game.js
+++ js/core/game.js
@@ -14,11 +14,10 @@ var Game = new function() {
 			if ( objects[ object ].update !== undefined )
 				objects[ object ].update( frame );
 		}
-		
 		for ( var object in marked_for_removal )
 			delete objects[ object ];
 		marked_for_removal = [];
-	} );
+	}, stage );
 	
 	// Adds an object
 	this.add = function( key, object ) {
@@ -44,14 +43,17 @@ var Game = new function() {
 		return objects[ key ];
 	};
 	
+	// Gets the stage
 	this.getStage = function() {
 		return stage;
 	};
 	
+	// Gets a layer
 	this.getLayer = function( layer ) {
 		return layers[ layer ];
 	};
 	
+	// Gets the center of the stage
 	this.getCenterStage = function() {
 		return { 
 			x: this.getStage().getWidth() / 2, 
@@ -59,11 +61,13 @@ var Game = new function() {
 		};
 	};
 	
+	// Starts the game
 	this.start = function() {
 		Input.start();
 		loop.start();
 	};
 	
+	// Stops the game
 	this.stop = function() {
 		Input.stop();
 		loop.stop();
--- js/models/hud/fps.js
+++ js/models/hud/fps.js
@@ -1,40 +1,29 @@
 var FPS_Logic = function( stage, layer ) {
 
 	// Add FPS text
-	this.text = new Kinetic.Text({
+	var text = new Kinetic.Text({
 		width: stage.getWidth() - 11,
 		y: 10,
-		text: '60 FPS',
+		text: '?? FPS',
 		fontSize: 8,
 		fontFamily: 'Arial',
 		fill: 'white',
 		align: 'right'
 	});
-	layer.add( this.text );
-
-	this.frameCount = 0;
-	this.frameRate = 0;
-	this.time = 0;
 	
-	// Create animation loop
-	var context = this;
-	this.animation = new Kinetic.Animation( function( frame ) {
-		context.update( frame );
-	}, layer);
+	layer.add( text );
+
+	var frameCount = 0;
+	var frameRate = 0;
+	var time = 0;
 	
-	this.animation.start();
-};
-
-FPS_Logic.prototype = {
-
-	// Once a second updates the FPS counter
-	update : function( frame ) {	
-		if ( ( this.time += frame.timeDiff ) >= 1000 ) {
-			this.frameRate = this.frameCount;
-			this.frameCount = 0;
-			this.time -= 1000;
-			this.text.setText( this.frameRate + ' FPS' );
+	this.update = function( frame ) {	
+		if ( ( time += frame.timeDiff ) >= 1000 ) {
+			frameRate = frameCount;
+			frameCount = 0;
+			time -= 1000;
+			text.setText( frameRate + ' FPS' );
 		}
-		this.frameCount++;
-	}
+		frameCount++;
+	};
 };
