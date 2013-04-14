082a4d355a5c25c9a3733f062c8c906cdcaec69f
Derk-Jan Karrenbeld <derk-jan+git@karrenbeld.info>
2013-04-15 01:02:53 +0200

    Remove background tiling code

--- js/glue/background.js
+++ js/glue/background.js
@@ -7,18 +7,5 @@
 	var layer = Game.getLayer( 'background' );
 	island_one.attach( layer );
 	island_two.attach( layer );
-	island_three.attach( layer );
-	
-	var backgroundImage = new Image();
-	backgroundImage.src = 'images/1945.water.gif';
-	
-	/*
-	var canvas = layer.getCanvas();
-	var context = layer.getContext();
-	var pattern = context.createPattern( backgroundImage, 'repeat' );
-
-	context.rect(0, 0, canvas.width, canvas.height);
-	context.fillStyle = pattern;
-	context.fill();
-	*/
+	island_three.attach( layer );	
 })();
